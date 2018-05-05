aa4f9d737bb6ab97e3b1e093ca0cbf1604eedd02
Erik Beltran <ebeltran@bcp.com.pe>
2018-05-04 19:38:23 -0500

    merge service response

--- main.cr
+++ main.cr
@@ -1,6 +1,6 @@
 require "option_parser"
 require "yaml"
-
+require "json"
 
 
 arg_port=9999
@@ -36,10 +36,49 @@ end
 
 pp request_table
 
+########################################### filters
+
+def filter_return(method,url)
+	response = HTTP::Client.exec method, url
+	return response.body.to_s
+end
+def filter_noreturn(method,url)
+	response = HTTP::Client.exec method, url
+	return ""
+end
+def filter_parallel(method,url)
+	response = HTTP::Client.exec method, url
+	return "response"
+end
+
+
+
+
+
+
+def filter_invoke(r_method, r_uri, r_filter)
+	case r_filter
+	when "parallel"
+		return filter_parallel(r_method, r_uri)
+	when "noreturn"
+		return filter_noreturn(r_method, r_uri)
+	else
+		return filter_return(r_method, r_uri)
+	end
+end
+
+
+
+##################################################
+
 require "http/server"
 server = HTTP::Server.new(arg_bind, arg_port.to_i) do |context|
 	key="#{context.request.method}:#{context.request.resource}"
-	pp key 
+	pp key
+	prepends = ""
+	response = ""
+	value = Hash(JSON::Any,JSON::Any).new
+	responseJson = JSON.parse("[]")
 	pp "response: "
 	chain = request_table[key] rescue 404
 	if chain == 404
@@ -50,13 +89,22 @@ server = HTTP::Server.new(arg_bind, arg_port.to_i) do |context|
 			definition = reqs.to_s.split(":")
 			r_method = definition[0]
 			r_uri = definition[1]
-			r_concat = definition[2] rescue "yes"
-			response = HTTP::Client.exec r_method, arg_host+r_uri
-			pp response.body 
-			pp response.status_code
+			r_filter = definition[2] rescue "return"
+#			response += filter_invoke(r_method, arg_host+r_uri, r_filter)
+			puts r_method, arg_host+r_uri, r_filter
+#			value = JSON.parse( "["+filter_invoke(r_method, arg_host+r_uri, r_filter)+"]" )
+#			puts value[0]
+			prepends = JSON.parse( filter_invoke(r_method, arg_host+r_uri, r_filter) )
+#			puts value["ws.user"]
 #			context.response.print "method: #{r_method}, uri: #{r_uri}, concat: #{r_concat}"
+			prepends.each do |key, xvalue|
+				value[key] = xvalue
+			end
 		end
-		context.response.print "ALL OK!"
+#		puts value
+		#puts JSON.parse(value)
+		context.response.print value
+#		context.response.print "ALL OK!"
 	end
 end
 
